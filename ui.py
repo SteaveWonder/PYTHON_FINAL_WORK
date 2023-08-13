c4026e16d93c26bcb80dd3e7990856a57985d0f8
Stepan <soulsteff@gmail.com>
2023-08-13 17:12:38 +0300

    ui

---
+++ ui.py
@@ -0,0 +1,25 @@
+import note
+
+
+def create_note(number):
+    title = check_len_text_input(
+        input('Insert Name of note: '), number)
+    body = check_len_text_input(
+        input('Insert Description of note: '), number)
+    return note.note(title=title, body=body)
+
+
+def menu():
+    print("\nThis is NoteApp. Functions:\n\n1 - Output all notes\n2 - Add notes\n3 - Delete notes\n4 - Edit notes\n5 - choose notes by the date\n6 - Shoe notes with it ID\n7 - Exit\n\nInsert function number: ")
+
+
+def check_len_text_input(text, n):
+    while len(text) <= n:
+        print(f'Text should be bigger than {n} symbols\n')
+        text = input('Insert text: ')
+    else:
+        return text
+
+
+def goodbuy():
+    print("Thank you! Goodbye!")
